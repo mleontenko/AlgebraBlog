50e67987fa7d0ef992e492e7745aafef277b4d0b
programeri <programeri@ucione.local>
2017-05-05 18:34:00 +0200

    update index.blade.php

--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -1,13 +1,17 @@
-<!DOCTYPE html>
-<html lang="hr">
-    <head>
-		<meta charset="utf-8">
-		<title>AlgebraBlog</title>
-    </head>
-    <body>
-		@foreach ($items as $item)
-			<h2><a href="items/{{ $item->id }}">{{ $item->title }}</a></h2>
-			<div>{{ $item->content }}</div>
-		@endforeach
-    </body>
-</html>
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+			@if ( count($items) > 0)
+				@foreach ($items as $item)
+					<h2><a href="items/{{ $item->id }}">{{ $item->title }}</a></h2>
+				@endforeach
+			@else
+				<h2>Nema zapisa u Blogu!</h2>
+			@endif
+		</div>
+    </div>
+</div>
+@endsection
